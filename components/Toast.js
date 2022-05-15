430f2a35330fb9352ce3ed54908aaf2dc96f332e
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2022-05-15 10:01:26 +0200

    fix: setup project

---
+++ components/Toast.js
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+
+
+const Toast = ({message, visible = true, success = false}) => {
+  const [_visible, setVisibility] = useState(visible);
+
+  useEffect(() => {
+    if (visible) {
+      const timer = setTimeout(() => {
+        setVisibility(false);
+      }, 3000);
+      return timer;
+    }
+  }, [visible])
+
+  if (!_visible) {
+    return null
+  }
+
+  return (
+    <div className="top right ui toast-container">
+      <div className="toast-box transition visible">
+        <div className={`${success ? 'pink' : 'green'} ui toast`} >
+          <div className="content">
+            <div>{message}</div>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
+};
+
+
+export default Toast
